1502c82c9dbb328918d745aa4ad44df1b5a90c99
Ayan Mamun <mamun.ayan@outlook.com>
2025-11-24 22:46:31 +0000

    Added username recovery route (ui)

--- recipes/views/password_reset_views.py
+++ recipes/views/password_reset_views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
-from recipes.forms import PasswordResetRequestForm
+from recipes.forms import PasswordResetRequestForm, UsernameResetRequestForm
 from recipes.views.decorators import LoginProhibitedMixin
 
 
@@ -33,4 +33,34 @@ class PasswordResetDoneView(LoginProhibitedMixin, TemplateView):
     """Show a neutral confirmation after a reset request is submitted."""
 
     template_name = 'password_reset_done.html'
+    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
+
+
+class UsernameResetRequestView(LoginProhibitedMixin, FormView):
+    """
+    Collect the user's email address to help recover their username.
+
+    The flow mirrors the password reset experience and keeps responses neutral
+    to avoid leaking account existence.
+    """
+
+    template_name = 'username_reset_request.html'
+    form_class = UsernameResetRequestForm
+    success_url = reverse_lazy('username_reset_done')
+    redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
+
+    def form_valid(self, form):
+        """
+        Accept the email and proceed to the confirmation step.
+
+        Hook for future delivery of username reminder can be added here.
+        """
+
+        return super().form_valid(form)
+
+
+class UsernameResetDoneView(LoginProhibitedMixin, TemplateView):
+    """Show a neutral confirmation after a username reset request."""
+
+    template_name = 'username_reset_done.html'
     redirect_when_logged_in_url = settings.REDIRECT_URL_WHEN_LOGGED_IN
