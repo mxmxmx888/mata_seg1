a377a293c1df2a1f981907e3d38c0aa7ab4affde
amirrza777 <80536453+amirrza777@users.noreply.github.com>
2025-12-10 00:25:00 +0000

    Worked On Shop UI and improved functionality

--- recipes/views/shop_view.py
+++ recipes/views/shop_view.py
@@ -4,9 +4,5 @@ from recipes.models import Ingredient
 
 @login_required
 def shop(request):
-    """
-    Display a gallery of ingredients that have shopping links.
-    """
     items = Ingredient.objects.filter(shop_url__isnull=False).exclude(shop_url__exact='').select_related('recipe_post').order_by('-id')
-    
     return render(request, 'shop.html', {'items': items})
